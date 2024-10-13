35631f777847beb29b91e4f5f1ae57f86b85a911
Olena_Chovgun <olenkachovgun@gmail.com>
2024-10-13 16:22:41 +0300

    sale

--- js/practice-2.js
+++ js/practice-2.js
@@ -35,15 +35,39 @@
 // напиши функцію яка сумуватиме сусідні числа і пушити в новий масив
 // const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
 
-const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
 
-function sum(someArr) {
-  const newArr = [];
+// function sum(someArr) {
+//   const newArr = [];
 
-  for (let i = 0; i < someArr.length - 1; i++) {
-    const newItem = someArr[i] + someArr[i + 1];
-    newArr.push(newItem);
-  }
-  return newArr;
-}
-console.log(sum(someArr));
+//   for (let i = 0; i < someArr.length - 1; i++) {
+//     const newItem = someArr[i] + someArr[i + 1];
+//     newArr.push(newItem);
+//   }
+//   return newArr;
+// }
+// console.log(sum(someArr));
+//---------
+//Напиши функцію яка призначає знижку 20% на фрукти в масиві,
+//Присвоїти ID для кожного продукту
+
+// function getRandomEightDigitNumber() {
+//     return Math.floor(10000000 + Math.random() * 90000000);
+// }
+// const fruits = [
+//   { name: "apple", price: 200 },
+//     { name: "orange", price: 300 },
+//     { name: "grapes", price: 750 },
+// ];
+// function sale(arr) {
+//   const newArr = [];
+//   for (const item of arr) {
+//     const newObj = {...item};
+//     newObj.price *= 0.8;
+//     newObj["id"] = getRandomEightDigitNumber();
+//     newArr.push(newObj);
+//   }
+//   return newArr
+// }
+// console.log(sale(fruits));
+
