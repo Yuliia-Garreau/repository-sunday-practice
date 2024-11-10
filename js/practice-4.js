9d762af1bb0380133ed5f929528024afd2cf7bb7
Olena_Chovgun <olenkachovgun@gmail.com>
2024-11-10 15:44:07 +0200

    task-3

--- js/practice-4.js
+++ js/practice-4.js
@@ -17,13 +17,65 @@
 // Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
 // */
 
-const btn = document.querySelector("#swapButton");
-const leftInput = document.querySelector("#leftSwapInput");
-const rightInput = document.querySelector("#rightSwapInput");
+// const btn = document.querySelector("#swapButton");
+// const leftInput = document.querySelector("#leftSwapInput");
+// const rightInput = document.querySelector("#rightSwapInput");
 
-btn.addEventListener("click", function change(event) {
-  const left = leftInput.value;
-  const right = rightInput.value;
-  leftInput.value = right;
-  rightInput.value = left;
-});
+// btn.addEventListener("click", function change(event) {
+//   const left = leftInput.value;
+//   const right = rightInput.value;
+//   leftInput.value = right;
+//   rightInput.value = left;
+// });
+/*
+Завдання 3
+Кнопка "Приховати" ховає текст і замінює назву кнопки на
+"Розкрити", при повторному натисканні текст знову стає доступним
+і кнопка набуває початкового вигляду.
+*/
+
+// const btn = document.querySelector("#passwordButton");
+// btn.addEventListener("click", handleclick);
+// const input = document.querySelector("#passwordInput");
+// function handleclick() {
+//   if (input.getAttribute("type") === "text") {
+//     input.setAttribute("type", "password");
+//     btn.textContent = "Розкрити";
+//     return
+//   }
+//     input.setAttribute("type", "text");
+//   btn.textContent = "Приховати";
+
+// }
+/*
+Завдання 4
+Кнопка "Зменшити" робить квадрат менше на 10 пікселів, допопка "Збільшити" - більше на 10 пікселів.
+https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+*/
+const box = document.querySelector("#box");
+const decrease = document.querySelector("#decrease");
+decrease.addEventListener("click", decreaseBox);
+const increase = document.querySelector("#increase");
+increase.addEventListener("click", increaseBox);
+let boxWidth = parseInt(getComputedStyle(box).width);
+let boxHight = parseInt(getComputedStyle(box).height);
+
+console.log(boxWidth);
+console.log(boxHight);
+
+function decreaseBox() {
+  if (boxWidth < 10 || boxHight < 10) {
+    return;
+  }
+  boxWidth -= 10;
+  boxHight -= 10;
+  box.style.width = `${boxWidth}px`;
+  box.style.height = `${boxHightpx}px`;
+}
+
+function increaseBox() {
+  boxWidth += 10;
+  boxHight += 10;
+  box.style.width = `${boxWidth}px`;
+  box.style.height = `${boxHightpx}px`;
+}
